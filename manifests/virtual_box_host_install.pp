831ce74e57f23f0d624cfdfad225c428334c7158
Your Name <you@example.com>
2014-12-10 04:30:09 +0100

    listing packages.

--- manifests/virtual_box_host_install.pp
+++ manifests/virtual_box_host_install.pp
@@ -1,13 +1,30 @@
 # See: http://www.if-not-true-then-false.com/2010/install-virtualbox-with-yum-on-fedora-centos-red-hat-rhel/
-binutils gcc make patch libgomp glibc-headers glibc-devel kernel-headers kernel-devel dkms
+#binutils gcc make patch libgomp glibc-headers glibc-devel kernel-headers kernel-devel dkms
 
-yum install VirtualBox-4.3
+$arPackageList = [
+  "binutils",
+  "gcc",
+  "make",
+  "patch",
+  "libgomp",
+  "glibc-headers",
+  "glibc-devel",
+  "kernel-headers",
+  "kernel-devel",
+  "dkms"
+                 ]
+package { $arPackageList: }
 
-/etc/init.d/vboxdrv setup
+#yum install VirtualBox-4.3
+# VirtualBox-4.3-4.3.18_96516_fedora18-1.x86_64.rpm 
+# exec { } for wget.
+# rpm the file. (Also conditional exec) or can package be made to get files localy?
+
+#/etc/init.d/vboxdrv setup
 ## OR ##
-service vboxdrv setup
+#service vboxdrv setup
 
 
-usermod -a -G vboxusers user_name
+#usermod -a -G vboxusers user_name
 
 
