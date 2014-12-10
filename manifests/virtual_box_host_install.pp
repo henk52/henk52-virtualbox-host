09a8db1f3f64a6e6ac18bac7673405829a9afe8f
Your Name <you@example.com>
2014-12-10 21:16:14 +0100

    Added install instructions for the VBox rpm.

--- manifests/virtual_box_host_install.pp
+++ manifests/virtual_box_host_install.pp
@@ -19,6 +19,23 @@ package { $arPackageList: }
 # VirtualBox-4.3-4.3.18_96516_fedora18-1.x86_64.rpm 
 # exec { } for wget.
 # rpm the file. (Also conditional exec) or can package be made to get files localy?
+$szVboxFedoraRpmFileName = 'VirtualBox-4.3-4.3.18_96516_fedora18-1.x86_64.rpm'
+
+exec { 'get_VBoxFedoraRpm':
+  creates => "/tmp/$szVboxFedoraRpmFileName",
+  command => "/usr/bin/wget --directory-prefix=/tmp http://10.1.233.3:/storage/$szVboxFedoraRpmFileName",
+}
+
+  
+exec { 'install_VBoxFedoraRpm':
+  creates => '/opt/DUDE',
+  command => "rpm -ivh /tmp/$szVboxFedoraRpmFileName",
+  require => [
+               File [ "/tmp/$szVboxFedoraRpmFileName" ],
+               Package [ $arPackageList ],
+             ],
+}
+
 
 #/etc/init.d/vboxdrv setup
 ## OR ##
