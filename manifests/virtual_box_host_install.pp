e581f51ebd7773b76bbfd105276fd1185cd4d32b
Your Name <you@example.com>
2014-12-20 13:07:05 +0100

    Now it also installs the vagrant tool.

--- manifests/virtual_box_host_install.pp
+++ manifests/virtual_box_host_install.pp
@@ -21,6 +21,8 @@ package { $arPackageList: }
 # rpm the file. (Also conditional exec) or can package be made to get files localy?
 $szVboxFedoraRpmFileName = 'VirtualBox-4.3-4.3.18_96516_fedora18-1.x86_64.rpm'
 
+$szVagrantLinuxRpmFileName = 'vagrant_1.6.3_x86_64.rpm'
+
 exec { 'get_VBoxFedoraRpm':
   creates => "/tmp/$szVboxFedoraRpmFileName",
   command => "/usr/bin/wget --directory-prefix=/tmp http://10.1.233.3:/storage/$szVboxFedoraRpmFileName",
@@ -39,6 +41,21 @@ exec { 'install_VBoxFedoraRpm':
 }
 #               File [ "/tmp/$szVboxFedoraRpmFileName" ],
 
+exec { 'get_VagrantLinuxRpm':
+  creates => "/tmp/$szVagrantLinuxRpmFileName",
+  command => "/usr/bin/wget --directory-prefix=/tmp http://10.1.233.3:/storage/$szVagrantLinuxRpmFileName",
+  path => [ '/usr/bin' ],
+}
+
+exec { 'install_VagrantLinuxRpm':
+  creates => '/usr/bin/vagrant',
+  command => "yum localinstall --assumeyes /tmp/$szVagrantLinuxRpmFileName",
+  path => [ '/usr/bin' ],
+  require => [
+               Exec [ 'get_VagrantLinuxRpm', 'install_VBoxFedoraRpm' ],
+               Package [ $arPackageList ],
+             ],
+}
 
 #/etc/init.d/vboxdrv setup
 ## OR ##
